0a7e04b051403a66df9b1f7b7efe8a13f840664d
Chirag-Devda <devdachirag09@gmail.com>
2024-06-07 18:52:19 +0530

    Completed Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
-
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
+import { Animated } from "react-animated-css";
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+        <div>
+          <Animated animationIn="zoomInDown" isVisible={true}>
+            <h1 className={`${styles.heroHeadText} text-white `}>
+              Hi I'm <span className="text-[#915eff]">Chirag</span>
+            </h1>
+          </Animated>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I am MERN Stack Developer
+            <br className="sm:block" hidden />i can make website with all new
+            technology
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas />
+      <div className="absolute xs:bottom-10 bottom-14 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-white flex justify-center items-start p-2">
+            <motion.dev
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-white mb-1"
+            />
+          </div>
+        </a>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
