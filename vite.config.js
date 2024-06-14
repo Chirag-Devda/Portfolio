2a955fe40cffca06be34993ca37bd85d8ec6c2f6
Chirag-Devda <devdachirag09@gmail.com>
2024-06-14 19:31:37 +0530

    Done

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 1600, // Adjust this value as needed
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Specify manual chunks here if necessary
+        },
+      },
+    },
+  },
+});
